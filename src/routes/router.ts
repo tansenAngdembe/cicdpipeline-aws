d1580ff57e29c0e995caf0db0d75de511b4c384a
tansenAngdembe <tansena54ang@gamil.com>
2026-06-20 10:46:14 +0545

    Ci/CD auto deployment ECR-ECS

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "Hello from Docker v1 🎉" }));
-// router.get("/ci/cd", (req, res) => res.json({ message: "Hello from Docker v1 CI/CD Pipeline 🎉" }));
+router.get("/", (req, res) => res.json({ message: "Hello from Docker v1 cicd 🎉" }));
+router.get("/ci/cd", (req, res) => res.json({ message: "Hello from Docker v1 CI/CD Pipeline 🎉" }));
 
 router.get("/health", (req, res) => {
-  // throw new Error("Internal Server Error");
   res.status(200).json({ message: "Everything is good here 👀" });
+  // throw new Errorim("Internal Server Error");
 });
 
 export default router;
+  
